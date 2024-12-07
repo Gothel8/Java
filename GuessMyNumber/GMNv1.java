1bec69f3d6de4630240eee42b8f491e7a0604f4b
Nurdan <150366666+NurdanTahsin@users.noreply.github.com>
2024-12-07 14:19:29 +0300

    Add files via upload

---
+++ GuessMyNumber/GMNv1.java
@@ -0,0 +1,27 @@
+import javax.swing.*;
+import java.util.*;
+
+public class GMNv1 {
+    public static void main(String[] args) {
+        int i = 1;
+        Random r = new Random();
+        int num = r.nextInt(10);
+
+        while(true){
+            String guess = JOptionPane.showInputDialog(null,"Guess my number!","GUESS MY NUMBER",JOptionPane.QUESTION_MESSAGE);
+            int g = Integer.parseInt(guess);
+
+            // ıf you say guess==num, you'll get an error.
+            // u supposed to compare same type variables
+
+            if(g==num){
+                String message = "Congratulations! You found my number in "+i+" attempts.";
+                JOptionPane.showMessageDialog(null,message,"YOU DID IT!",JOptionPane.INFORMATION_MESSAGE);
+                break;
+            }
+            else
+                i++;
+
+        }
+    }
+}
