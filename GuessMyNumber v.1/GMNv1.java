ae6a33db4cab73b214b71693bdf67cb17af43c42
Nurdan <150366666+NurdanTahsin@users.noreply.github.com>
2024-12-07 14:16:53 +0300

    Add files via upload

--- GuessMyNumber v.1/GMNv1.java
+++ GuessMyNumber v.1/GMNv1.java
@@ -1,23 +1,27 @@
+import javax.swing.*;
 import java.util.*;
 
 public class GMNv1 {
     public static void main(String[] args) {
-        int sayac = 1;
-        Random r = new Random ();
-        int random = r.nextInt(10);
-        Scanner sc = new Scanner (System.in);
+        int i = 1;
+        Random r = new Random();
+        int num = r.nextInt(10);
 
-        System.out.println("\n----- WELCOME TO THE GAME -----\n\n");
         while(true){
-            System.out.println("Guess my number! (0-10)");
-            int tahmin = sc.nextInt();
+            String guess = JOptionPane.showInputDialog(null,"Guess my number!","GUESS MY NUMBER",JOptionPane.QUESTION_MESSAGE);
+            int g = Integer.parseInt(guess);
 
-            if(tahmin==random){
-                System.out.println("Congratulations! You found my number in "+sayac+" attempts.");
+            // ıf you say guess==num, you'll get an error.
+            // u supposed to compare same type variables
+
+            if(g==num){
+                String message = "Congratulations! You found my number in "+i+" attempts.";
+                JOptionPane.showMessageDialog(null,message,"YOU DID IT!",JOptionPane.INFORMATION_MESSAGE);
                 break;
             }
             else
-                sayac += 1;
+                i++;
+
         }
     }
 }
